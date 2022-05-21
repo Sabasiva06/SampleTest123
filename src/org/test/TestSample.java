9391586286826ab40fd856d38f3b5b88a1d063f2
Sabasiva06 <sabasiva06@gmail.com>
2022-05-21 12:59:45 +0530

    sample list code

---
+++ src/org/test/TestSample.java
@@ -0,0 +1,20 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSample {
+
+	public static void main(String[] args) {
+
+		List<String> li = new ArrayList<>();
+		li.add("add");
+		li.add("sub");
+		li.add("Multiply");
+		li.add("division");
+		li.add("percentage");
+		li.add("modulus");
+
+	}
+
+}
